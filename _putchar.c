0b274c8e226a92a9c01feb3f2696e6c97448a736
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-10-17 10:39:25 -0400

    Create vital supporting functions

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: 0 when successfull, else, -1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
