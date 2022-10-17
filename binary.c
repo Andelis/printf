23f426f831b62a53b58ed796a8bafb6d470ad3d3
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-10-17 10:45:40 -0400

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_binary - print the binary representation of a number
+ * @n: number to be printed in binary
+ * @printed: hold the number of characters printed
+ */
+
+void print_binary(unsigned int n, unsigned int *printed)
+{
+	if (n > 1)
+	{
+		*printed += 1;
+		print_binary(n >> 1, printed);
+	}
+	_putchar((n & 1) + '0');
+}
