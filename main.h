b0c49f6006be2e08f6751ed6bdd08f4c7a7642c9
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-10-17 10:45:05 -0400

    Modify programs

--- main.h
+++ main.h
@@ -1,12 +1,34 @@
 #ifndef _PRINTF_H_
 #define _PRINTF_H_
 
-#include <stdlib.h>
 #include <stdarg.h>
 
-/* _printf */
-int _printf(const char *format, ...);
-
 int _putchar(char c);
+int print_int(va_list arg);
+int print_unsigned(va_list arg);
+int _printf(const char *format, ...);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(void);
+void print_binary(unsigned int n, unsigned int *printed);
+int print_unsignedToBinary(va_list arg);
+int print_oct(va_list arg);
+int print_unsignedIntToHex(unsigned int num, char _case);
+int print_hex_base(va_list arg, char _case);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
+int print_STR(va_list arg);
+
+/**
+ * struct identifierStruct - structure definition
+ * @indentifier: type
+ * @printer: function to print
+ */
+
+typedef struct identifierStruct
+{
+char *indentifier;
+int (*printer)(va_list);
+} identifierStruct;
 
 #endif /* _PRINTF_H_ */
